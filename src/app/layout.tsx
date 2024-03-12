0974474e395dcfd52a9a50f19f356e643c5ea81f
Nguyễn Nam Hoàng <112799623+nnhoanghd2004@users.noreply.github.com>
2024-03-13 01:28:13 +0700

    final v2.1 add some meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,11 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
-  title: 'RMIT Business Club',
+  title: 'RMIT Vietnam Business Club - SGS',
   description: 'Founded in 2004, making us the oldest active academic club at RMIT with the vision of nurturing future business leaders.',
   icons: 'https://firebasestorage.googleapis.com/v0/b/rmit-bc-web-storage.appspot.com/o/logo%20black.png?alt=media&token=c0577899-9f7e-476e-9103-3f57783a06e5',
   openGraph: {
-    title: 'RMIT Business Club',
+    title: 'RMIT Vietnam Business Club - SGS',
     description: 'Founded in 2004, making us the oldest active academic club at RMIT with the vision of nurturing future business leaders.',
     type: 'website',
     images: [`https://firebasestorage.googleapis.com/v0/b/rmit-bc-web-storage.appspot.com/o/%5BBC%5D%20Website%20Cover.png?alt=media&token=fc6c6e1d-ab37-45c5-85e8-1f9519477afe`]
